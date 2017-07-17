4fa2ec5e654a6e7a9e09330fb48a99d12e4db057
Anton Kovalchuk <koval400k@gmail.com>
2017-07-17 22:11:43 +0200

    TCPEchoClient

---
+++ TCPEchoClient/TCPEchoClient/DieWithMessage.c
@@ -0,0 +1,21 @@
+//
+//  DieWithMessage.c
+//  TCPEchoClient
+//
+//  Created by Anton Kovalchuk on 16.04.17.
+//  Copyright © 2017 Anton Kovalchuk. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "DieWithMessage.h"
+
+void DieWithUserMessage(const char *msg, const char *detail) {
+	printf("%s: %s\n",msg,detail);
+    exit(1);
+}
+
+void DieWithSystemMessage(const char *msg) {
+    perror(msg);
+    exit(1);
+}
