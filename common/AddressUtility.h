7522468006106603e16eb5478065f1aa7969aba3
Anton Kovalchuk <koval400k@gmail.com>
2017-07-18 22:26:57 +0200

    Common utilities

---
+++ common/AddressUtility.h
@@ -0,0 +1,3 @@
+#include <stdio.h>
+
+void PrintSocketAddress(const struct sockaddr *address, FILE *stream);
---
+++ common/TCPClientUtility.h
@@ -0,0 +1,10 @@
+//
+//  TCPClientUtility.h
+//  GenericTCPClient
+//
+//  Created by Anton Kovalchuk on 17.04.17.
+//  Copyright © 2017 Anton Kovalchuk. All rights reserved.
+//
+#include <stdio.h>
+
+int SetupTCPClientSocket(const char *host, const char *service);
