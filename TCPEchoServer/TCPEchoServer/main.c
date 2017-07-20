66e4530bdeabcd4d31f14e5ba192efe063da5f04
Anton Kovalchuk <koval400k@gmail.com>
2017-07-20 23:06:00 +0200

    TCP clint/server fixes

--- TCPEchoServer/TCPEchoServer/main.c
+++ TCPEchoServer/TCPEchoServer/main.c
@@ -15,11 +15,9 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include "DieWithMessage.h"
+#include "../../common/TCPServerUtility.h"
 
 static const int MAXPENDING = 5; // Maximum outstanding connection requests
-static const int BUFSIZE = 1024;
-
-void HandleTCPClient(int clntSocket);
 
 int main(int argc, const char * argv[]) {
     
@@ -69,32 +67,3 @@ int main(int argc, const char * argv[]) {
     
     return 0;
 }
-
-void HandleTCPClient(int clntSocket) {
-    
-    char buffer[BUFSIZE]; // Buffer for echo string
-    
-    // Receive message from client
-    ssize_t numBytesRcvd = recv(clntSocket, buffer, BUFSIZE, 0);
-    if (numBytesRcvd < 0)
-        DieWithSystemMessage("recv() failed");
-
-    // Send received string and receive again until end of stream
-    while (numBytesRcvd > 0) { // 0 indicates end of stream
-        // Echo message back to client
-        ssize_t numBytesSent = send(clntSocket, buffer, numBytesRcvd, 0);
-        if (numBytesSent < 0)
-            DieWithSystemMessage("send() failed");
-        else if (numBytesSent != numBytesRcvd)
-            DieWithUserMessage("send()", "sent unexpected number of bytes");
-        
-        // See if there is more data to receive
-        numBytesRcvd = recv(clntSocket, buffer, BUFSIZE, 0);
-        if (numBytesRcvd < 0)
-            DieWithSystemMessage("recv() failed");
-    }
-    
-    close(clntSocket); // Close client socket
-}
-
-
